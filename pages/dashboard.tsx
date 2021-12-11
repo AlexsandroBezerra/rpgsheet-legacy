89421854f44a6ee0b3fb727c752d5295383460e0
Sandro Bezerra <alexsandro.g.bezerra@gmail.com>
2021-12-11 14:12:30 -0300

    feat: sign in with github

---
+++ pages/dashboard.tsx
@@ -0,0 +1,25 @@
+import { signOut, useSession } from "next-auth/react"
+import Head from "next/head"
+
+function Home() {
+  const { data: session } = useSession()
+
+  return (
+    <>
+      <Head>
+        <title>Dashoard | rpgsheet</title>
+      </Head>
+
+      <h1>Dashboard</h1>
+      <h2>
+        Signed with: {session?.user?.name} - {session?.user?.email}
+      </h2>
+
+      <button type="button" onClick={() => signOut({ callbackUrl: "/" })}>
+        Sign out
+      </button>
+    </>
+  )
+}
+
+export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,44 @@
+import { signIn, useSession } from "next-auth/react"
 import Head from "next/head"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 function Home() {
+  const { replace } = useRouter()
+  const { data: session, status } = useSession()
+
+  // useEffect(() => {
+  //   if (session) {
+  //     replace("/dashboard")
+  //   }
+  // }, [session, replace])
+
+  // if (status === "loading") {
+  //   return (
+  //     <>
+  //       <Head>
+  //         <title>rpgsheet</title>
+  //       </Head>
+
+  //       <h1>carregando...</h1>
+  //     </>
+  //   )
+  // }
+
   return (
     <>
-    <Head>
-      <title>rpgsheet</title>
-    </Head>
-    
-    <h1>rpgsheet</h1>
+      <Head>
+        <title>rpgsheet</title>
+      </Head>
+
+      <h1>rpgsheet</h1>
+
+      <button
+        type="button"
+        onClick={() => signIn("github", { callbackUrl: "/dashboard" })}
+      >
+        Sign in with github
+      </button>
     </>
   )
 }
