d7913103384befa72c9c9e457afec3c98ba916b2
Sandro Bezerra <alexsandro.g.bezerra@gmail.com>
2021-12-11 15:51:08 -0300

    feat: handle sign in errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,9 @@
-import { signIn, useSession } from "next-auth/react"
 import Head from "next/head"
+import { signIn } from "next-auth/react"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
 
 function Home() {
-  const { replace } = useRouter()
-  const { data: session, status } = useSession()
-
-  // useEffect(() => {
-  //   if (session) {
-  //     replace("/dashboard")
-  //   }
-  // }, [session, replace])
-
-  // if (status === "loading") {
-  //   return (
-  //     <>
-  //       <Head>
-  //         <title>rpgsheet</title>
-  //       </Head>
-
-  //       <h1>carregando...</h1>
-  //     </>
-  //   )
-  // }
+  const { query } = useRouter()
 
   return (
     <>
@@ -39,6 +19,8 @@ function Home() {
       >
         Sign in with github
       </button>
+
+      {query.error && "Erro ao fazer login"}
     </>
   )
 }
