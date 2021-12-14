d7e46ef985a5da2b6d78c429cafc62b024c863e9
Sandro Bezerra <alexsandro.g.bezerra@gmail.com>
2021-12-14 08:45:42 -0300

    chore: add ceo

---
+++ src/components/seo.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+
+export function SEO() {
+  return (
+    <Head>
+      <meta name="title" content="rpgsheet - Crie as fichas dos seus personagens de RPG" />
+      <meta
+        name="description"
+        content="Com o rpgsheet, você cria fichas de personagens e usa as fichas em sessões de RPG presenciais com o celular, ou sessões online direto do computador."
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="rpgsheet - Crie as fichas dos seus personagens de RPG" />
+      <meta
+        property="og:description"
+        content="Com o rpgsheet, você cria fichas de personagens e usa as fichas em sessões de RPG presenciais com o celular, ou sessões online direto do computador."
+      />
+      <meta
+        property="og:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="rpgsheet - Crie as fichas dos seus personagens de RPG" />
+      <meta
+        property="twitter:description"
+        content="Com o rpgsheet, você cria fichas de personagens e usa as fichas em sessões de RPG presenciais com o celular, ou sessões online direto do computador."
+      />
+      <meta
+        property="twitter:image"
+        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+      />
+    </Head>
+  )
+}
