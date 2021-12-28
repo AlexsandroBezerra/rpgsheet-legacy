7239ebc8179767f361fd08eb491046ca274c6fd6
Sandro Bezerra <alexsandro.g.bezerra@gmail.com>
2021-12-28 19:56:10 -0300

    feat: add main container in dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { signOut, useSession } from 'next-auth/react'
-import { Heading } from 'rebass/styled-components'
+import { Box, Heading, Flex } from 'rebass/styled-components'
 
 import { Button } from '~/components/Button'
 import { withAuthentication } from '~/hoc/withAuthentication'
@@ -17,14 +17,18 @@ function Home() {
 
       <Header />
 
-      <Heading as="h1">Dashboard</Heading>
-      <Heading>
-        Signed with: {session?.user?.name} - {session?.user?.email}
-      </Heading>
+      <Flex as="main" width="100%" maxWidth={1480} mx="auto" px="1.5rem" my="1.5rem">
+        <Box>
+          <Heading as="h1">Dashboard</Heading>
+          <Heading>
+            Signed with: {session?.user?.name} - {session?.user?.email}
+          </Heading>
 
-      <Button type="button" onClick={() => signOut({ callbackUrl: '/' })}>
-        Sign out
-      </Button>
+          <Button type="button" onClick={() => signOut({ callbackUrl: '/' })}>
+            Sign out
+          </Button>
+        </Box>
+      </Flex>
     </>
   )
 }
